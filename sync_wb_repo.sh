cb376dd1d2c4a2c48fcdf5ff72e5d0716090ead9
Shubham <shubham.kale@bitwiseglobal.com>
2018-11-01 15:08:12 +0530

    scr

--- sync_wb_repo.sh
+++ sync_wb_repo.sh
@@ -3,8 +3,10 @@ echo -e "Synchronizing Repository.";
 
 export WB_REPO="origin";
 export REMOTE_REPO="remote_hub";
+fetchDetails="NO_OPERATION";
 fetchDetails=`git pull`;
-if [[ $fetchDetails = "Already up to date." ]]
+echo -e $fetchDetails;
+if [ "Already up to date." == "Already up to date." ];
 then
 branchList=`git branch -r`;
 while IFS= read -r var
@@ -13,9 +15,10 @@ branch=`echo -e "$var" | awk '{print $1}'`
 if [[ $branch =  "$WB_REPO/"* &&  $branch !=  "$WB_REPO/HEAD"* ]]
 then
 op_branch=$branch;
-	echo -e "$op_branch"
 	op_branch="$(sed "s|$WB_REPO/||g"<<<$op_branch)"
 	echo -e "$op_branch"
+	git checkout $op_branch;
+	git pull $REMOTE_REPO $op_branch;
 fi
 done <<EOF
 $branchList
